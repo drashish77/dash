9d9c1f73a7fcf74f060f58c4f4fc963702be82c5
Ashish Gupta <ashish.gupta@squareboat.com>
2024-02-29 04:49:32 +0530

    chore:added dark and light mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import { Suspense } from "react";
 import Loading from "./components/Loading";
 import Final from "./components/Final";
+import { ModeToggle } from "./components/DarkModeToggleButton";
+import ThemeSwitch from "./components/Switch";
 
 export default function Home() {
   return (
-    <main className="container m-auto">
+    <main className="container m-auto relative ">
+      {/* <ModeToggle /> */}
+      <div className="absolute right-2 -top-10 md:-top-5">
+        <button>
+          <ThemeSwitch />
+        </button>
+      </div>
       <Suspense fallback={<Loading />}>
         <Final />
       </Suspense>
